a1400246ab35a72e008ef78989a1b3c812e4f5f0
kaushikraghupathruni <kaushikraghupathruni@gmail.com>
2020-05-02 21:19:48 +0530

    first c++ program

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Saitama! ";
+    return 0;
+}
+
+// Compile this program
+// g++ -std=c++14 -o hello.exe hello.cpp
+
+// Run this program
+// hello.exe
