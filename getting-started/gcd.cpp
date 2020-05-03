aa938810bad199dbb76abdf90f5f8bced975aa89
kaushikraghupathruni <kaushikraghupathruni@gmail.com>
2020-05-03 08:57:58 +0530

    gcd non-recursive

---
+++ getting-started/gcd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+    return a;
+}
+
+int main() {
+    int x, y;
+    cout << "Enter two integers: ";
+    if (!(cin >> x >> y)) {
+        cout << "Please enter only integers" << endl;
+    } else {
+        cout << "gcd of " << x << " and " << y << " is: " << gcd(x, y) << endl;
+    }
+    return 0;
+}
